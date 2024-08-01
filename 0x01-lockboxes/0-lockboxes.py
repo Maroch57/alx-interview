6fb7e80fe82b295b122c94798f30dbeb455ba403
Maroch57 <jahathblessed@gmail.com>
2024-08-02 00:52:58 +0300

    implementation of lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+This script demonstrates the implementation of a lockbox problem.
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    Unlock given lockboxes.
+
+    Args:
+        boxes (list): A list of lockboxes, where each box is
+        represented by a boolean value.
+
+    Returns:
+        list: A list of unlocked boxes.
+    """
+    box_count = len(boxes)
+    visited = [False] * box_count
+    visited[0] = True
+    stack = [0]
+
+    while stack:
+        box = stack.pop()
+        for key in boxes[box]:
+            if key < box_count and not visited[key]:
+                visited[key] = True
+                stack.append(key)
+
+    return all(visited)
